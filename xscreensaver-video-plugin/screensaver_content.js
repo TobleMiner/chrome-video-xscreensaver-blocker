6a72f9310fedee67017e707a7cd002ca28343a76
Tobias Schramm <tobleminer@gmail.com>
2017-09-07 22:33:41 +0200

    Make content script more robust by using various page load events

--- xscreensaver-video-plugin/screensaver_content.js
+++ xscreensaver-video-plugin/screensaver_content.js
@@ -5,20 +5,34 @@ let screensaverChannel = chrome.runtime.connect({name: 'screensaver_channel'});
 // Global video element id. Acts as a tab-level uniqe id
 let videoId = 0;
 
+let videos = [];
+
 //$$('video').forEach(function(video) {
-let videoTags = document.getElementsByTagName('video');
-for(let i = 0; i < videoTags.length; i++)
+function collectVideoTags()
 {
-	let video = videoTags[i];
-	screensaverChannel.postMessage({type: "register", videoId: videoId});
-	video.addEventListener('play', function(video) {
-		screensaverChannel.postMessage({type: "play",
-			videoId: videoId, timestamp: Date.now()});
-	});
-	video.addEventListener('pause', function(video) {
-		screensaverChannel.postMessage({type: "pause",
-			videoId: videoId, timestamp: Date.now()});
-	});
-	videoId++;
+	let videoTags = document.getElementsByTagName('video');
+	for(let i = 0; i < videoTags.length; i++)
+	{
+		let video = videoTags[i];
+		if(videos.indexOf(video) !== -1)
+			continue;
+
+		screensaverChannel.postMessage({type: "register", videoId: videoId});
+		video.addEventListener('play', function(video) {
+			screensaverChannel.postMessage({type: "play",
+				videoId: videoId, timestamp: Date.now()});
+		});
+		video.addEventListener('pause', function(video) {
+			screensaverChannel.postMessage({type: "pause",
+				videoId: videoId, timestamp: Date.now()});
+		});
+		videoId++;
+		videos.push(video);
+	}
 }
 //});
+
+document.addEventListener('DOMContentLoaded', collectVideoTags);
+window.addEventListener('load', collectVideoTags);
+
+collectVideoTags();
