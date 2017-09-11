c389fad9e4c228c0c1c1fd647092f9200a52e64f
Tobias Schramm <tobleminer@gmail.com>
2017-09-11 07:46:47 +0200

    Prevent unblock vs do_block timer race by restructuring blocking code

--- xscreensaver-video-server/server.py
+++ xscreensaver-video-server/server.py
@@ -3,41 +3,38 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse
 from threading import Timer
-from os import execl, fork
 from subprocess import call
 
 SCREENSAVER_PORT = 23433
 
 class ScreensaverBlockHandler():
+	def __init__(self):
+		self.blocked = False
+
 	def block(self):
-		raise NotImplementedError()
+		print('Screensaver blocked')
+		self.blocked = True
 
 	def unblock(self):
-		raise NotImplementedError()
+		print('Screensaver unblocked')
+		self.blocked = False
 
 class XScreensaverBlockHandler(ScreensaverBlockHandler):
 	UNBLOCK_INTERVAL = 30
 
 	def __init__(self, xscr_cmd_bin='/usr/bin/xscreensaver-command'):
+		super().__init__()
 		self.xscr_cmd_bin = xscr_cmd_bin
 		self.xscr_cmd_timer = None
-
-	def block(self):
-		print('Xscreensaver blocked')
 		self.do_block()
 
 	def do_block(self):
 		self.xscr_cmd_timer = Timer(XScreensaverBlockHandler.UNBLOCK_INTERVAL, self.do_block)
-		exit_code = call(self.xscr_cmd_bin + ' -time | grep -q non-blanked', shell=True)
-		if(exit_code == 0):
-			call(self.xscr_cmd_bin + ' -deactivate', shell=True)
+		if(self.blocked):
+			exit_code = call(self.xscr_cmd_bin + ' -time | grep -q non-blanked', shell=True)
+			if(exit_code == 0):
+				call(self.xscr_cmd_bin + ' -deactivate', shell=True)
 		self.xscr_cmd_timer.start()
-
-	def unblock(self):
-		print('Xscreensaver unblocked')
-		if(self.xscr_cmd_timer):
-			self.xscr_cmd_timer.cancel();
-			self.xscr_cmd_timer = None
 
 class ScreensaverRequestHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
